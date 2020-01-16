fd4d9851a9218c9b9034ac0ccfa73baab6193b72
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-16 06:46:48 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -10,9 +10,6 @@ def join_ingredients(src)
 end
 
 
-# src = [[-1, -900], [10, 30], [0, 0], [14, 16 * -2.5], [Math.sin(1), 19]]
-# return [-1, 30, 0, 14, 19]
-
 def find_greater_pair(src)
   
   count=0
@@ -29,6 +26,10 @@ def find_greater_pair(src)
   final
 end
 
+array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
+ # if both numbers in the pair are even, then add both those numbers to the
+  # total
+return 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
