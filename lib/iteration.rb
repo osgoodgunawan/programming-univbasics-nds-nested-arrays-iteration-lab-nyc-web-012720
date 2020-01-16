5d64175d384282c4f696e1fee52333ddf49f5a1c
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-16 06:54:37 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,44 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  final=[]
+  count=0
+ element_index=0
+  while count<src.size do
+    final.push( "I love " + src[count][element_index] +" and "+ src[count][element_index+1] +" on my pizza", )
+    count+=1
+  end
+  final
 end
+
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  
+  count=0
+  element_index=0
+  final=[]
+  while count<src.length do 
+    if src[count][element_index]>src[count][element_index+1] 
+        final.push(src[count][element_index])
+    else 
+      final.push(src[count][element_index+1])
+    end
+    count +=1
+  end
+  final
 end
 
+# array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
+ # if both numbers in the pair are even, then add both those numbers to the
+  # total 
+# return 344812
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  total=0
+  count=0
+  element_index=0
+while count<src.length do
+  if src[count][element_index] %2==0 && src[count][element_index+1]%2==0
+    total=src[count][element_index]+src[count][element_index+1]+total
+  end
+  count+=1
+end
+total
 end
